dbc1433c27ca5fcfd82d3d5e3dd935e9183ca224
rajan_03@hotmail.com <rajanshrestha80@gmail.com>
2021-02-10 09:48:57 -0600

    finished Project

--- script.js
+++ script.js
@@ -4,22 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
     var accusation = 1;
     for (var i = 1; i < 101; i++) {
         var h3 = document.createElement("h3");
-        h3.textContent = "accusation" + [i];
+        h3.innerText = "accusation" + [i];
         document.body.appendChild(h3);
-        h3.addEventListener("click", function () {
-            alert(cal(i));
-        });
+        var index = addIndex(i);
+        h3.addEventListener("click", index);
     }
 
-    var friend_Name = ["A", "B", "C", "D", "E"];
-    var location_Name = ["F", "G", "H", "I", "J", "A", "B", "C", "D", "E"];
-    var weapon_Name = ["F", "G", "H", "I", "J", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "A", "B", "C", "D", "E"];
-    function cal(i) {
-
-       alert (`Accusation ${i}: I accuse ${friend_Name}, with the ${weapon_Name} in the ${location_Name}!`)
+    var friend_Name = ["Ken", "Sam", "Josh", "Jake", "Drake"];
+    var location_Name = ["France", "Germany", "Hoover", "birmingham", "Jasper", "Atlanta", "Baltimore", "Colorado", "Denver", "East Coast"];
+    var weapon_Name = ["Knife", "Gun", "Hammer", "pen", "computer", "desk", "Boat", "Rifel", "Bomb", "Snipergit ", "F", "G", "H", "I", "J", "A", "B", "C", "D", "E"];
+    
+    function addIndex(index) {
+        return function () {
+            alert (`Accusation ${index}: I accuse ${friend_Name[index % 5]}, with the ${weapon_Name[index % 10]} in the ${location_Name[index % 20]}!`)
+        }
     }
-
-
 })
 
 
